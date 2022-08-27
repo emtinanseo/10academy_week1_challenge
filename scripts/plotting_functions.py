37b22dddeb69e1a8426b9da7911304a4b6a507a1
Emtinan <emtinan.s.e.osman@gmail.com>
2022-08-27 13:15:15 +0200

    Modified the script of plotting_functions

--- scripts/plotting_functions.py
+++ scripts/plotting_functions.py
@@ -4,21 +4,24 @@ import matplotlib.pyplot as plt
 
 ###################################PLOTTING FUNCTIONS###################################
 
-def plot_hist(df:pd.DataFrame, column:str, color:str, file_name:str)->None:
+def plot_hist(df:pd.DataFrame, column:str, color:str, file_name= None)->None:
     # plt.figure(figsize=(15, 10))
     # fig, ax = plt.subplots(1, figsize=(12, 7))
     sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
     plt.title(f'Distribution of {column}', size=20, fontweight='bold')
-    plt.savefig(file_name)
+    if file_name:
+        plt.savefig(file_name)
     plt.show()
 
-def plot_count(df:pd.DataFrame, column:str) -> None:
+def plot_count(df:pd.DataFrame, column:str, file_name= None) -> None:
     plt.figure(figsize=(12, 7))
     sns.countplot(data=df, x=column)
     plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+    if file_name:
+        plt.savefig(file_name)
     plt.show()
     
-def plot_bar(df:pd.DataFrame, x_col:str, y_col:str, title:str, xlabel:str, ylabel:str)->None:
+def plot_bar(df:pd.DataFrame, x_col:str, y_col:str, title:str, xlabel:str, ylabel:str, file_name= None)->None:
     plt.figure(figsize=(12, 7))
     sns.barplot(data = df, x=x_col, y=y_col)
     plt.title(title, size=20)
@@ -26,33 +29,43 @@ def plot_bar(df:pd.DataFrame, x_col:str, y_col:str, title:str, xlabel:str, ylabe
     plt.yticks( fontsize=14)
     plt.xlabel(xlabel, fontsize=16)
     plt.ylabel(ylabel, fontsize=16)
+    if file_name:
+        plt.savefig(file_name)
     plt.show()
 
-def plot_heatmap(df:pd.DataFrame, title:str, cbar=False)->None:
+def plot_heatmap(df:pd.DataFrame, title:str, cbar=False, file_name= None)->None:
     plt.figure(figsize=(12, 7))
     sns.heatmap(df, annot=True, cmap='viridis', vmin=0, vmax=1, fmt='.2f', linewidths=.7, cbar=cbar )
     plt.title(title, size=18, fontweight='bold')
+    if file_name:
+        plt.savefig(file_name)
     plt.show()
 
-def plot_box(df:pd.DataFrame, x_col:str, title:str) -> None:
+def plot_box(df:pd.DataFrame, x_col:str, title:str, file_name= None) -> None:
     plt.figure(figsize=(12, 7))
     sns.boxplot(data = df, x=x_col)
     plt.title(title, size=20)
     plt.xticks(rotation=75, fontsize=14)
+    if file_name:
+        plt.savefig(file_name)
     plt.show()
 
-def plot_box_multi(df:pd.DataFrame, x_col:str, y_col:str, title:str) -> None:
+def plot_box_multi(df:pd.DataFrame, x_col:str, y_col:str, title:str, file_name= None) -> None:
     plt.figure(figsize=(12, 7))
     sns.boxplot(data = df, x=x_col, y=y_col)
     plt.title(title, size=20)
     plt.xticks(rotation=75, fontsize=14)
     plt.yticks( fontsize=14)
+    if file_name:
+        plt.savefig(file_name)
     plt.show()
 
-def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
+def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str, file_name= None) -> None:
     plt.figure(figsize=(12, 7))
     sns.scatterplot(data = df, x=x_col, y=y_col, hue=hue, style=style)
     plt.title(title, size=20)
     plt.xticks(fontsize=14)
     plt.yticks( fontsize=14)
+    if file_name:
+        plt.savefig(file_name)
     plt.show()
