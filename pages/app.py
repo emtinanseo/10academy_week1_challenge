6db69008639fdf6058feaa99d265e849c9b86e07
Emtinan <emtinan.s.e.osman@gmail.com>
2022-08-28 04:16:50 +0200

    Overview analysis graphs added in dir outcomes

--- pages/app.py
+++ pages/app.py
@@ -1,19 +1,57 @@
 import streamlit as st
+import pandas as pd
+import sys
+sys.path.append("..")
 
-from multi_app import MultiApp
-import user_overview_analysis #, user_engagement, user_experience, , user_satisfaction # import your app modules here
+st.title("User Overview Analysis")
 
-app = MultiApp()
-
-st.sidebar.markdown('# **Tellco Customers Analysis**')
 st.sidebar.markdown("""
-Before investing on a business it is a must to have best understanding about the field. This project is all about analyzing TellCo's user and find out whether it is worth buying or selling.
-""")
+                    User Analytics in the
+                    Telecommunication Industry\n
+                    TellCo Customers\' Data
+                    """)
+
+st.markdown("The goal of this app is to give an overview of the dataset. It provides a preview of Telecom customers' data aggregated per user, as well as visualization of graphical univariate and bivariate analysis.")
+
+st.header('Dataset Aggregated by User')
+df = pd.read_csv('../outcomes/user_overview_analysis.csv')
+st.write(df)
+
+st.header('Preliminary Insights')
+st.subheader('Top 10 handsets used by the customers')
+st.image('../outcomes/top10handsettypes.jpg')
+
+st.subheader('Top 3 Handset Manufacturers')
+st.image('../outcomes/top3handsetmanufacturers.jpg')
+
+st.subheader('The top 5 handsets per top 3 handset manufacturer')
+st.markdown('1. Top 5 handset made by Apple company')
+st.image('../outcomes/topApplehandsets.jpg')
+st.markdown('2. Top 5 handset made by Samsung company')
+st.image('../outcomes/topSamsunghandsets.jpg')
+st.markdown('3. Top 5 handset made by Huawei company')
+st.image('../outcomes/topHuaweihandsets.jpg')
+
+st.header('Graphical Univariate Analysis')
+st.subheader('Number of Sessions Per User')
+st.image('../outcomes/distribution of num of sessions.jpg')
+
+
+##for multiapp usage
+##from multi_app import MultiApp
+#import user_overview_analysis #, user_engagement, user_experience, , user_satisfaction # import your app modules here
+
+#app = MultiApp()
+
+#st.sidebar.markdown('# **Tellco Customers Analysis**')
+#st.sidebar.markdown("""
+#Before investing on a business it is a must to have best understanding about the field. This #project is all about analyzing TellCo's user and find out whether it is worth buying or selling.
+#""")
 
 # Add all your application here
-app.add_app("User Overview", user_overview_analysis.app)
+#app.add_app("User Overview", user_overview_analysis.app)
 #app.add_app("User Engagement", user_engagement.app)
 #app.add_app("User Experience", user_experience.app)
 #app.add_app("User Satisfaction", user_satisfaction.app)
 # The main app
-app.run()
+#app.run()
--- scripts/plotting_functions.py
+++ scripts/plotting_functions.py
@@ -10,7 +10,7 @@ def plot_hist(df:pd.DataFrame, column:str, color:str, file_name= None)->None:
     sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
     plt.title(f'Distribution of {column}', size=20, fontweight='bold')
     if file_name:
-        plt.savefig(file_name)
+        plt.savefig(file_name, bbox_inches = 'tight')
     plt.show()
 
 def plot_count(df:pd.DataFrame, column:str, file_name= None) -> None:
@@ -18,7 +18,7 @@ def plot_count(df:pd.DataFrame, column:str, file_name= None) -> None:
     sns.countplot(data=df, x=column)
     plt.title(f'Distribution of {column}', size=20, fontweight='bold')
     if file_name:
-        plt.savefig(file_name)
+        plt.savefig(file_name, bbox_inches = 'tight')
     plt.show()
     
 def plot_bar(df:pd.DataFrame, x_col:str, y_col:str, title:str, xlabel:str, ylabel:str, file_name= None)->None:
@@ -30,7 +30,7 @@ def plot_bar(df:pd.DataFrame, x_col:str, y_col:str, title:str, xlabel:str, ylabe
     plt.xlabel(xlabel, fontsize=16)
     plt.ylabel(ylabel, fontsize=16)
     if file_name:
-        plt.savefig(file_name)
+        plt.savefig(file_name, bbox_inches = 'tight')
     plt.show()
 
 def plot_heatmap(df:pd.DataFrame, title:str, cbar=False, file_name= None)->None:
@@ -38,7 +38,7 @@ def plot_heatmap(df:pd.DataFrame, title:str, cbar=False, file_name= None)->None:
     sns.heatmap(df, annot=True, cmap='viridis', vmin=0, vmax=1, fmt='.2f', linewidths=.7, cbar=cbar )
     plt.title(title, size=18, fontweight='bold')
     if file_name:
-        plt.savefig(file_name)
+        plt.savefig(file_name, bbox_inches = 'tight')
     plt.show()
 
 def plot_box(df:pd.DataFrame, x_col:str, title:str, file_name= None) -> None:
@@ -47,7 +47,7 @@ def plot_box(df:pd.DataFrame, x_col:str, title:str, file_name= None) -> None:
     plt.title(title, size=20)
     plt.xticks(rotation=75, fontsize=14)
     if file_name:
-        plt.savefig(file_name)
+        plt.savefig(file_name, bbox_inches = 'tight')
     plt.show()
 
 def plot_box_multi(df:pd.DataFrame, x_col:str, y_col:str, title:str, file_name= None) -> None:
@@ -57,7 +57,7 @@ def plot_box_multi(df:pd.DataFrame, x_col:str, y_col:str, title:str, file_name= 
     plt.xticks(rotation=75, fontsize=14)
     plt.yticks( fontsize=14)
     if file_name:
-        plt.savefig(file_name)
+        plt.savefig(file_name, bbox_inches = 'tight')
     plt.show()
 
 def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str, file_name= None) -> None:
@@ -67,5 +67,5 @@ def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str,
     plt.xticks(fontsize=14)
     plt.yticks( fontsize=14)
     if file_name:
-        plt.savefig(file_name)
+        plt.savefig(file_name, bbox_inches = 'tight')
     plt.show()
